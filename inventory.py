c70ad759555a6dd1c9247ed620e57b62b0bcd45f
OliverEklund <oliver.eklund@elev.ga.ntig.se>
2025-11-12 13:42:31 +0100

    grunken har hittats

--- inventory.py
+++ inventory.py
@@ -2,10 +2,11 @@ from abc import ABC, abstractmethod
 from typing import List
 
 class Item:
-    def __init__(self, name):
+    def __init__(self, name, spaces = 0, uses = 0):
         self.name = name
+    @abstractmethod
     def use(self):
-        pass
+        print("Du använder" + self.name)
 
 class Lead_pipe(Item):
     def use(self):
@@ -14,17 +15,33 @@ class Lead_pipe(Item):
 class Inventory():
     def __init__(self):
         self.contents = []
+        spaces = 20
     
     def get_contents(self):
         return self.contents
     
-    @abstractmethod
     def add_item(self, item):
         self.contents.append(item)
+    
+    def remove_item(self, item):
+        if item in self.contents:
+            self.contents.remove(item)
+        else:
+            pass
 
 player_inventory = Inventory()
-hagelbössa = Item("hagelbössa")
+hagelbössa = Item("Hagelbössa", 7, 2)
+wienerbröd = Item("Stort Wienerbröd", 1, 5)
+Sten = Item("Sten", 2, 1)
 player_inventory.add_item(hagelbössa)
+player_inventory.add_item(Sten)
+player_inventory.add_item(wienerbröd)
 
 for item in player_inventory.get_contents():
-    print(item)
+    print(item.name)
+
+input("")
+player_inventory.remove_item(hagelbössa)
+
+for item in player_inventory.get_contents():
+    print(item.name)
