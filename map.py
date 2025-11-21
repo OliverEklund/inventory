6a1ab1aa88ad9e338e2d37f60878450498762546
OliverEklund <oliver.eklund@elev.ga.ntig.se>
2025-11-21 11:15:58 +0100

    asdas

--- map.py
+++ map.py
@@ -1,5 +1,15 @@
-map_data = [0,0,0,0,1,0,0,0,0]
+# map_data = [0,0,0,0,1,0,0,0,0]
+map_data = [
+    [0,0,0,1,0],
+    [0,1,0,0,1]
+]
 
 def map_print():
-    for i in range(0, len(map_data), 3):
-        print(map_data[i:i+3])
+    # for i in range(0, len(map_data), 3):
+    #     print(map_data[i:i+3])
+    for i in map_data:
+        print(i)
+        # for j in i:
+        #     print(j)
+
+map_print()
